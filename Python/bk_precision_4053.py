85bebf9624ee427aebe7f7fbeb41e6b949ff52d8
Adam Turowski <adam.turowski@argondesign.com>
2016-12-07 10:03:56 +0000

    Removed some unnecessary code. Started rearranging the code

---
+++ Python/bk_precision_4053.py
@@ -0,0 +1,134 @@
+
+class BkPrecision4053:
+  """
+  Class implementing methods allowing to use py-visa to work with
+  BK Precision 4053 Arbitrary Waveform Generator
+  """
+  
+  def __init__(self, rm, rm_open_string):
+    """
+    Initialization of the instrument. The function
+    requires two arguments:
+    rm - py-visa Resource Manager instance
+    rm_open_string - the string required to open communication with 4053 in Resource Manager
+    """
+    self.instrument = rm.open_resource(rm_open_string)
+    self.instrument_open = True
+    
+    self.instrument.write_termination = "\n"  # by default the commands should be ended with single Line Feed character
+
+  # check if correct instrument is connected
+    idn_string = self.instrument.query("*IDN?")
+    if (idn_string.find("BK Precision,4053") == -1):
+      raise ValueError("BK Precision 4053 AWG is not detected")
+    
+  def __enter__(self):
+    """
+    Object method called when entering the block surrounded by Python "with" statement
+    """
+    return self
+    
+  def __exit__(self, exc_type, exc_value, traceback):
+    """
+    Method called when exiting the block surrounded by Python "with" statement.
+    Please note that it will also be called if Python exception has been thrown.
+    This fact allow us to clean up after ourselves
+    """
+    if (self.instrument_open == True):
+      self.instrument.close()
+      self.instrument_open = False
+    
+  def __del__(self):
+    """
+    Method called when the object is deleted. Allows for cleaning up after ourselves
+    """
+    if (self.instrument_open == True):
+      self.instrument.close()
+      self.instrument_open = False
+
+  def is_connected(self):
+    """
+    Method checks if correct instrument is connected
+    """
+    if (idn_string.find("BK Precision,4053") != -1):
+      return True
+    else
+      return False
+      
+  def identify(self):
+    """
+    Method queries the instrument to find out details about what instrument is connected
+    """
+    print (self.instrument.query("PROD MODEL?"))
+    print (self.instrument.query("IDN-SGLT-PRI?"))
+    print (self.instrument.query("PROD BAND?"))
+
+  def define_arbitrary_waveform(self, index, data, name = None, freq_hz = 1000.0, amp_v = 5.0, offset_v = 0.0, phase_deg = 0.0):
+  
+  # check if given arguments values are correct
+    if ((index < 0) or (index > 9)):
+      raise ValueError("Arbitrary waveform index {0:d} is outside <0,9> range".format(index))
+    
+    if (len(data) != 16384):
+      raise ValueError("Arbitrary waveform data lenght = {0:d} bytes has to be equal to 16384".format(len(data)))
+    
+    if (name != None):
+      if not (type(name) is str):
+        raise ValueError("Arbitrary waveform name has to be a string")
+      elif (len(name) > 5)):
+        raise ValueError("Arbitrary waveform name can be up to 5 characters long")
+
+    if ((name != None) and (len(name) > 5)):
+      raise ValueError("Arbitrary waveform name can be up to 5 characters long")
+    
+    if (freq_hz < 0.0):
+      raise ValueError("Arbitrary waveform frequency can't be negative")
+    elif (freq_hz > 10000000.0):
+      raise ValueError("Arbitrary waveform frequency can't be more than 10MHz")
+    
+    if (amp_v <= 0.0):
+      raise ValueError("Arbitrary waveform amplitude has to be positive")
+    elif (amp_v > 10.0):
+      raise ValueError("Arbitrary waveform amplitude can't be more than 10V")
+    
+    if ((offset_v < -5.0) or (offset_v > 5.0)):
+      raise ValueError("Arbitrary waveform offset has to be within (-5V, +5V) range")
+    
+    if ((phase_deg < -360.0) or (phase_deg > 360.0)):
+      raise ValueError("Arbitrary waveform phase has to be withing <-360, 360> degrees range")
+      
+  # arguments are fine, so prepare their string version in the right format
+    mem_index_str = "M{0:02d}".format(index + 50)
+    if (name == None):
+    # create default name if necessary
+      name_str = "wave{0:1d}".format(index)
+    else:
+    # name was provided, so make sure that it is 5 characters long
+      name_str = name
+      if (len(name_str) < 5):
+        added_spaces = 5-len(name_str)
+        for i in range(added_spaces):
+          name_str.join(" ")
+
+    freq_str = "{0:.9f}".format(freq_hz)
+    amp_str = "{0:.9f}".format(amp_v)
+    offset_str = "{0:.9f}".format(offset_v)
+    phase_str =  "{0:.9f}".format(phase_deg)
+    
+  # prepare the command for the generator
+    cmmd = "WVDT " + mem_index_str + ","
+    cmmd += "WVNM," + name_str + ","
+    cmmd += "TYPE,5,LENGTH,32KB,"
+    cmmd += "FREQ," + freq_str + ","
+    cmmd += "AMPL," + amp_str + ","
+    cmmd += "OFST," + offset_str + ","
+    cmmd += "PHASE," + phase_str + ","
+    cmmd += "WAVEDATA,"
+  
+  # and the waveform data
+  # first find the maximum data value and scale the whole set accordingly
+  
+  
+    
+    
+    
